7f6f48cc3410f6356201ec37ccee8597fac76998
ItsChaco26 <aaronchacon2601@gmail.com>
2025-06-21 00:46:27 -0600

    Config kafka authentication

---
+++ authentication-service/src/main/java/ucr/ac/cr/authentication/service/KafkaConsumerService.java
@@ -0,0 +1,35 @@
+package ucr.ac.cr.authentication.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import ucr.ac.cr.authentication.api.types.RegisterUserPymeDTO;
+import ucr.ac.cr.authentication.jpa.entities.UserPymeEntity;
+import ucr.ac.cr.authentication.jpa.repositories.UserPymeRepository;
+
+@Service
+public class KafkaConsumerService {
+
+    @Autowired
+    private UserPymeRepository repository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @KafkaListener(topics = "register-user", groupId = "auth-service")
+    public void listen(String message) {
+        try {
+            RegisterUserPymeDTO dto = objectMapper.readValue(message, RegisterUserPymeDTO.class);
+
+            UserPymeEntity entity = new UserPymeEntity();
+            entity.setUserId(dto.userId());
+            entity.setPymeId(dto.pymeId());
+
+            repository.save(entity);
+            System.out.println("Guardado en base de datos: " + dto.userId() + " -> " + dto.pymeId());
+
+        } catch (Exception e) {
+            System.err.println("Error al procesar mensaje Kafka: " + e.getMessage());
+        }
+    }
+}
