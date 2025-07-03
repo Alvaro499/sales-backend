b9a3dd5e6ec2b50a5d57d44be36c34d1b14baff2
Aarón Chacón Céspedes <116319804+ItsChaco26@users.noreply.github.com>
2025-07-02 22:55:20 -0600

    Update CorsConfig.java

--- ventas-back/src/main/java/ucr/ac/cr/BackendVentas/config/CorsConfig.java
+++ ventas-back/src/main/java/ucr/ac/cr/BackendVentas/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // permite todas las rutas
-                .allowedOrigins("http://localhost:5173") // permite todos los orígenes
+                .allowedOrigins("*")  // permite todos los orígenes
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // métodos permitidos
                 .allowedHeaders("*"); // permite todos los headers
     }
